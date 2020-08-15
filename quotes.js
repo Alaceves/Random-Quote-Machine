600d190a9c7ef0ade264fc0fe8308d9d4f3a54cb
Alaceves <zeroalonter@gmail.com>
2020-08-15 06:08:01 -0500

    my first commit

---
+++ quotes.js
@@ -0,0 +1,38 @@
+const QUOTEBANK = [
+  {
+  quote:"Sometimes I pull so hard I rip the skin.",
+  author:"-Brad McGuire"},
+{
+  quote:"Slaves, Get your ass back here!",
+  author:"-Van Darkholme"},
+{
+  quote:"I'm an artist, I'm a performance artist.",
+  author:"-Van Darkholme"},
+{
+  quote:"That's power son, That's power!",
+  author:"-Billy Herrington"},
+{
+  quote:"Ok, Throw me some numbers.",
+  author:"-Steve Rambo"},
+{
+  quote:"You got me mad now.",
+  author:"-Billy Herrington"},
+{
+  quote:"You got me mad now.",
+  author:"-Billy Herrington"},
+{
+  quote:"Ho ho ho, Ganging up!?",
+  author:"-Danny Lee"},
+{
+  quote:"Ready for the next round?",
+  author:"-Mark Wolf"},
+{
+  quote:"How do you like that? How do you like that, man?",
+  author:"-Duncan Mills"},
+{
+  quote:"On my shoulder!",
+  author:"-Billy Herrington"},
+{
+  quote:"You're a scrappy dog, aren't ya?",
+  author:"-T.J. Cummings"},
+]
